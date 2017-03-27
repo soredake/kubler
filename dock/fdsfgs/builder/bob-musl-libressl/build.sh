6268ae8d70b105218fa665ce0ea4aefa819d30f2
soredake <fdsfgs@krutt.org>
2017-03-27 17:11:43 +0300

    27/03/2017

--- dock/fdsfgs/builder/bob-musl-libressl/build.sh
+++ dock/fdsfgs/builder/bob-musl-libressl/build.sh
@@ -1,62 +1,46 @@
 #
-# build config, sourced by build-root.sh inside build container
+# Kubler phase 1 config, pick installed packages and/or customize the build
 #
-PACKAGES=""
-EMERGE_BIN="emerge"
 
 #
-# this hook can be used to configure the build container itself, install packages, etc
+# This hook can be used to configure the build container itself, install packages, run any command, etc
 #
 configure_bob() {
-    # install flaggie, required for update_use() helper
-    emerge app-portage/flaggie
-    mv /etc/portage/package.accept_keywords /etc/portage/foo
-    mkdir -p /etc/portage/package.{accept_keywords,unmask,mask,use}
-    mv /etc/portage/foo /etc/portage/package.accept_keywords/flaggie
+    fix_portage_profile_symlink
+    # install basics used by helper functions
+    emerge app-portage/flaggie app-portage/eix app-portage/gentoolkit
+    configure_eix
+    # migrate from files to directories at /etc/portage/package.*
+    for i in /etc/portage/package.{accept_keywords,unmask,mask,use}; do
+        [[ -f "${i}" ]] && { cat "${i}"; mv "${i}" "${i}".old; }
+        mkdir -p "${i}"
+        [[ -f "${i}".old ]] &&  mv "${i}".old "${i}"/default
+    done
     touch /etc/portage/package.accept_keywords/flaggie
-
-
+    echo 'LANG="en_US.UTF-8"' > /etc/env.d/02locale
+    env-update
+    source /etc/profile
     # libressl
     echo 'USE="${USE} libressl"' >> /etc/portage/make.conf
     echo "-libressl" >> /etc/portage/profile/use.stable.mask
     echo "-curl_ssl_libressl" >> /etc/portage/profile/use.stable.mask
     mask_package dev-libs/openssl
-    update_keywords 'dev-libs/libressl' '+**'
+    echo "dev-libs/libressl **" >> /etc/portage/package.accept_keywords/flaggie
     emerge -f libressl
     rm /usr/lib/misc/ssh-keysign
     emerge -C openssh openssl
     emerge -1q libressl
     emerge -1q wget python:2.7 python:3.4
-
-    # set locale of build container
-    #echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
-    # not supported with musl
-    #locale-gen
-    echo 'LANG="en_US.UTF-8"' > /etc/env.d/02locale
-    env-update
-    source /etc/profile
     # install default packages
     update_use 'dev-vcs/git' '-perl'
     update_use 'app-crypt/pinentry' '+ncurses'
-    #update_keywords 'app-portage/layman' '+~amd64'
-    #update_keywords 'dev-python/ssl-fetch' '+~amd64'
+    update_keywords 'app-portage/layman' '+~amd64'
+    update_keywords 'dev-python/ssl-fetch' '+~amd64'
     update_use 'net-misc/curl' '+curl_ssl_libressl' '-curl_ssl_openssl'
-    #app-portage/layman
-    emerge net-misc/curl dev-vcs/git sys-devel/distcc app-misc/jq app-portage/eix
-    # init eix portage db
-    #eix-update
-    # setup layman
-    #sed -i 's/^check_official : Yes/check_official : No/g' /etc/layman/layman.cfg # no pesky prompts please
-    #layman -L
-    #layman -a musl
-    sync_overlay musl https://anongit.gentoo.org/git/proj/musl.git
-    eix-update
-}
-
-#
-# this hook is called in the build container just before tar'ing the rootfs
-#
-finish_rootfs_build()
-{
-    :
+    emerge net-misc/curl dev-vcs/git app-portage/layman sys-devel/distcc app-misc/jq
+    install_git_postsync_hooks
+    configure_layman
+    # add musl overlay, it may exist already in the shared portage container
+    layman -l | grep -q musl && layman -d musl
+    layman -a musl
 }
--- dock/fdsfgsglibcmultilib/builder/bob-multilib-libressl/build.sh
+++ dock/fdsfgsglibcmultilib/builder/bob-multilib-libressl/build.sh
@@ -1,18 +1,23 @@
 #
-# build config, sourced by build-root.sh inside build container
+# Kubler phase 1 config, pick installed packages and/or customize the build
 #
-PACKAGES=""
-EMERGE_BIN="emerge"
 
 #
-# this hook can be used to configure the build container itself, install packages, etc
+# This hook can be used to configure the build container itself, install packages, run any command, etc
 #
 configure_bob() {
-    # install flaggie, required for update_use() helper
-    emerge app-portage/flaggie
+    fix_portage_profile_symlink
+    # install basics used by helper functions
+    emerge app-portage/flaggie app-portage/eix app-portage/gentoolkit
+    configure_eix
     mkdir -p /etc/portage/package.{accept_keywords,unmask,mask,use}
     touch /etc/portage/package.accept_keywords/flaggie
-
+    # set locale of build container
+    echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+    locale-gen
+    echo 'LANG="en_US.UTF-8"' > /etc/env.d/02locale
+    env-update
+    source /etc/profile
     # libressl
     echo 'USE="${USE} libressl"' >> /etc/portage/make.conf
     echo "-libressl" >> /etc/portage/profile/use.stable.mask
@@ -24,32 +29,16 @@ configure_bob() {
     emerge -C openssh openssl
     emerge -1q libressl
     emerge -1q wget python:2.7 python:3.4
-
-    # set locale of build container
-    echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
-    locale-gen
-    echo 'LANG="en_US.UTF-8"' > /etc/env.d/02locale
-    env-update
-    source /etc/profile
     # install default packages
     update_use 'dev-vcs/git' '-perl'
     update_use 'app-crypt/pinentry' '+ncurses'
-    #update_keywords 'app-portage/layman' '+~amd64'
-    #update_keywords 'dev-python/ssl-fetch' '+~amd64'
+    update_keywords 'app-portage/layman' '+~amd64'
+    update_keywords 'dev-python/ssl-fetch' '+~amd64'
     update_use 'net-misc/curl' '+curl_ssl_libressl' '-curl_ssl_openssl'
-    #app-portage/layman
-    emerge net-misc/curl dev-vcs/git sys-devel/crossdev sys-devel/distcc app-misc/jq app-portage/eix
-    # init eix portage db
-    eix-update
-    # setup layman
-    #sed -i 's/^check_official : Yes/check_official : No/g' /etc/layman/layman.cfg # no pesky prompts please
-    #layman -L
-}
-
-#
-# this hook is called in the build container just before tar'ing the rootfs
-#
-finish_rootfs_build()
-{
-    :
+    emerge net-misc/curl dev-vcs/git app-portage/layman sys-devel/distcc app-misc/jq
+    install_git_postsync_hooks
+    configure_layman
+    # install aci/oci requirements
+    emerge dev-lang/go
+    install_oci_deps
 }
