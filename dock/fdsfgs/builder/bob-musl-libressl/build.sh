2428b62ee62a4a47e4307b2bc70e3ad95c5ecb1a
soredake <fdsfgs@krutt.org>
2017-03-27 17:27:41 +0300

    27/03/2017

--- dock/fdsfgs/builder/bob-musl-libressl/build.sh
+++ dock/fdsfgs/builder/bob-musl-libressl/build.sh
@@ -25,6 +25,8 @@ configure_bob() {
     echo "-libressl" >> /etc/portage/profile/use.stable.mask
     echo "-curl_ssl_libressl" >> /etc/portage/profile/use.stable.mask
     mask_package dev-libs/openssl
+    mask_package dev-python/cryptography::gentoo
+    sync_overlay libressl https://github.com/gentoo/libressl.git
     echo "dev-libs/libressl **" >> /etc/portage/package.accept_keywords/flaggie
     emerge -f libressl
     rm /usr/lib/misc/ssh-keysign
